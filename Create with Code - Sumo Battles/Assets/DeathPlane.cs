06b0438c4fdf6ee8134e9184a33efb5b0b9dd27a
abdulhakeemidr <56049093+abdulhakeemidr@users.noreply.github.com>
2022-12-22 00:32:15 -0600

    Added Death Plane

---
+++ Create with Code - Sumo Battles/Assets/DeathPlane.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) 
+    {
+        if(other.gameObject.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+        }
+
+        if(other.gameObject.CompareTag("Player"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}
